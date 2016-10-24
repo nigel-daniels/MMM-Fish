adab87932a21c717faec3541206e2e5ef83f4d56
nigel.daniels <nigel.daniels@axiomode.com>
2016-10-24 13:14:37 -0700

    Initial commit.

---
+++ MMM-Fish.js
@@ -0,0 +1,115 @@
+/* Magic Mirror Module: MMM-Fish
+ * Version: 1.0.0
+ *
+ * By Nigel Daniels https://github.com/nigel-daniels/
+ * MIT Licensed.
+ */
+
+Module.register('MMM-Fish', {
+
+
+    defaults: {
+        fish:   [
+                {'id':'anchovy','name':'Anchovy'},
+                {'id':'cod','name':'Cod'},
+                {'id':'dogfish','name':'Dogfish'},
+                {'id':'eel','name':'Eel'},
+                {'id':'haddock','name':'Haddock'},
+                {'id':'halibut','name':'Halibut'},
+                {'id':'herring','name':'Herring'},
+                {'id':'mackerel','name':'Mackerel'},
+                {'id':'monkfish','name':'Monkfish'},
+                {'id':'mullet','name':'Mullet'},
+                {'id':'plaice','name':'Plaice'},
+                {'id':'red_snapper','name':'Red Snapper'},
+                {'id':'salmon','name':'Salmon'},
+                {'id':'sardine','name':'Sardine'},
+                {'id':'sea_bass','name':'Sea Bass'},
+                {'id':'sea_bream','name':'Sea Bream'},
+                {'id':'skate','name':'Skate'},
+                {'id':'sole','name':'Sole'},
+                {'id':'swordfish','name':'Swordfish'},
+                {'id':'trout','name':'Trout'},
+                {'id':'tuna','name':'Tuna'},
+                {'id':'turbot','name':'Turbot'}
+                ],
+        interval:   3600000 // Every 60 mins
+        },
+
+
+        // Define required scripts.
+    	getScripts: function() {
+    		return ["moment.js"];
+    	},
+
+        getStyles: function() {
+            return ['fish.css', 'font-awesome.css'];
+            },
+
+    	// Define start sequence.
+    	start: function() {
+    		Log.info("Starting module: " + this.name);
+
+    		this.lastFish = -1;
+            this.day = -1;
+
+    		// Schedule update timer.
+    		var that = this;
+    		setInterval(function() {that.fishOfTheDay();}, this.config.interval);
+    	},
+
+
+    	randomFish: function() {
+            var that = this;
+
+    		var generate = function() {
+    			return Math.floor(Math.random() * that.config.fish.length);
+    		};
+
+    		var fishIndex = generate();
+
+    		while (fishIndex === this.lastFish) {
+    			fishIndex = generate();
+    		}
+
+    		this.lastFish = fishIndex;
+
+    		return this.config.fish[fishIndex];
+    	},
+
+
+    	fishOfTheDay: function(that) {
+            var date = new Date;
+            var day = date.getDay();
+
+            if (day != this.day) {
+                this.day = day;
+                this.updateDom(4000);
+            }
+    	},
+
+    	// Override dom generator.
+    	getDom: function() {
+
+    		var fish = this.randomFish();
+
+    		var id = fish.id;
+            var fishName = fish.name;
+
+    		var wrapper = document.createElement('div');
+    		wrapper.className = "bright fish";
+
+            var image = document.createElement('img');
+            image.className = 'fishPic';
+            image.src = './modules/MMM-Fish/images/' + id + '.png';
+
+            var name = document.createElement('span');
+            name.className = 'fishName';
+            name.innerHTML = fishName;
+
+            wrapper.appendChild(image);
+    		wrapper.appendChild(name);
+
+    		return wrapper;
+    	}
+    });
---
+++ README.md
@@ -0,0 +1,23 @@
+# MMM-Fish
+This is a module for the [MagicMirror](https://github.com/MichMich/MagicMirror/tree/develop). This module shows the fish of the day.
+
+## Installation
+1. Navigate into your MagicMirror's `modules` folder and execute `git clone https://github.com/nigel-daniels/MMM-Fish`.  A new folder `MMM-Fish` will appear.
+
+## Config
+The entry in `config.js` is simple and has no options.
+
+Here is an example of an entry in `config.js`
+```
+{
+    module:		'MMM-Fish',
+    header:		'Fish of the Day',
+    position:	'top_left'
+},
+```
+
+## Notes
+I was asked if we could have a fish of the day ^_^
+
+## Thanks To...
+- [Michael Teeuw](https://github.com/MichMich) for the [MagicMirror2](https://github.com/MichMich/MagicMirror/tree/develop) framework that made this module possible.
